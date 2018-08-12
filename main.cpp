6a6f16593d08d87d3dafaba4ef663c9d6d09433a
Oka Yoshiki <35644659+miuele@users.noreply.github.com>
2018-08-12 22:40:45 +0900

    add class X

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #define nameof(a) #a
 
+class Y
+{
+public:
+	Y &operator=(Y const &) & = default;
+	Y &operator=(Y const &) && = delete;
+}
+
 int main()
 {
 	std::cout << "hello world" << std::endl;
